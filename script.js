0c9f4244bb2e2a3b2095231c8242e3bd70769fb1
Samuel Peoples <peoples.sam@gmail.com>
2025-03-07 13:11:28 +1100

    Created calculator inteface

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(currentTotal, val){
+    return currentTotal += val;
+}
+
+function subtract (currentTotal, val){
+    return currentTotal -= val;
+}
+
+function divide(currentTotal, val){
+    return currentTotal / val;
+}
+
+function multiply(currentTotal,val){
+    return currentTotal * val;
+}
