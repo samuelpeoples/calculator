9f2a5246b5746016c638dc03a363123611009ce4
samuelpeoples <peoples.sam@gmail.com>
2025-03-15 22:46:01 +1100

    Added keyboard bindings and attempted to impliment back keys, stopping for now

--- script.js
+++ script.js
@@ -19,6 +19,26 @@ const screenHist = document.querySelector("#screen-hist");
 const screenHistText = document.createElement("span");
 screenHist.appendChild(screenHistText);
 
+document.addEventListener("keydown", (e) => {
+	if (e.key === "." || !isNaN(e.key)) {
+		updateValues(`${e.key}`);
+	} else if (e.key === "+") {
+		updateOperation("add");
+	} else if (e.key === "-") {
+		updateOperation("subtract");
+	} else if (e.key === "*") {
+		updateOperation("multiply");
+	} else if (e.key === "/") {
+		updateOperation("divide");
+	} else if (e.key === "=" || e.key === "Enter") {
+		operate(currentOperation, previousNum, currentNum);
+	} else if (e.key === "Delete") {
+		clear();
+	} else if (e.key === "Backspace") {
+		clear();
+	}
+});
+
 const buttonList = document.querySelectorAll("button").forEach((button) => {
 	button.addEventListener("click", () => {
 		let id = button.id;
@@ -30,37 +50,40 @@ const buttonList = document.querySelectorAll("button").forEach((button) => {
 			updateOperation(id);
 		} else if (id === "equals") {
 			operate(currentOperation, previousNum, currentNum);
-		} else {
+		} else if (id === "btnClear") {
 			clear();
+		} else if (id === "btnBack") {
+			back();
 		}
 	});
 });
 
 function clear() {
 	previousInput = "";
-    currentInput = [];
+	currentInput = [];
 	runningInput = [];
 
-    previousNum = null;
+	previousNum = null;
 	currentNum = null;
 	sum = null;
 
 	currentOperation = null;
-    previousOperation = null;
-    operationSymbol = "";
+	previousOperation = null;
+	operationSymbol = "";
 	screenHistText.textContent = "";
 	screenMainText.textContent = "";
 	console.clear();
 }
+
 const operations = {
-    add: (a,b) => a + b,
-    subtract:(a,b) => a - b,
-    multiply:(a,b) => a * b,
-    divide:(a,b) => (a !== 0 && b !== 0 ? a / b: "No")
-}
+	add: (a, b) => a + b,
+	subtract: (a, b) => a - b,
+	multiply: (a, b) => a * b,
+	divide: (a, b) => (a !== 0 && b !== 0 ? a / b : "No"),
+};
 
 function updateOperation(arg) {
-    if ( isNaN(previousInput || previousInput != ".")) return;
+	if (isNaN(previousInput || previousInput != ".")) return;
 
 	if (currentOperation != null) previousOperation = currentOperation;
 	switch (arg) {
@@ -85,10 +108,10 @@ function updateOperation(arg) {
 	if (previousOperation != null && !completedOperation)
 		operate(previousOperation, previousNum, currentNum);
 
-    if (currentNum != null) {
-        previousNum = currentNum;
-        currentNum = null
-    }
+	if (currentNum != null) {
+		previousNum = currentNum;
+		currentNum = null;
+	}
 
 	updateScreen(operationSymbol);
 	completedOperation = false;
@@ -112,8 +135,8 @@ function operate(operation, arg1, arg2) {
 	previousNum = sum;
 	currentNum = null;
 	completedOperation = true;
-	screenMainText.textContent = sum;
-	screenHistText.textContent += "=" + sum;
+	screenHistText.textContent = sum;
+	screenMainText.textContent += "=" + sum;
 	return sum;
 }
 
@@ -125,8 +148,8 @@ function updateScreen(char) {
 		if (runningInput.length != 1) runningInput.push("");
 	} else {
 		if (runningInput[length] != null) {
-            console.log("pop")
-			let newNum = (runningInput.pop());
+			console.log("pop");
+			let newNum = runningInput.pop();
 			newNum += char;
 			runningInput.push(newNum);
 			currentNum = newNum;
@@ -137,8 +160,8 @@ function updateScreen(char) {
 	}
 
 	previousInput = char;
-	screenHistText.textContent += previousInput;
-	screenMainText.textContent += char;
+	screenMainText.textContent += previousInput;
+	screenHistText.textContent += char;
 	console.log(`Previous ${previousNum}`);
 	console.log(`Current ${currentNum}`);
 }
